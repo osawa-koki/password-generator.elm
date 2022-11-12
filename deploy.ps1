a5f9912a1ecfa1d94ce8dfa08b6b9ecb5a8d186b
osawa-koki <github@osawa.dev>
2022-11-12 20:53:09 +0900

    デプロイスクリプト修正した。

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,12 @@
 
-Copy-Item -Path "./src/index.html" -Destination "./docs/index.html" -Recurse
-Copy-Item -Path "./src/style.scss" -Destination "./docs/style.scss" -Recurse
-Copy-Item -Path "./src/style.css" -Destination "./docs/style.css" -Recurse
+# 前回の分を削除
+Remove-Item -Path "./docs*.*" -Force -Recurse
+
+# 資産系をコピー
+Copy-Item -Path "./public/index.html" -Destination "./docs/index.html" -Recurse
+Copy-Item -Path "./public/style.scss" -Destination "./docs/style.scss" -Recurse
+Copy-Item -Path "./public/style.css" -Destination "./docs/style.css" -Recurse
+Copy-Item -Path "./public/favicon.ico" -Destination "./docs/favicon.ico" -Recurse
+
+# elmファイルのビルドとデプロイ
 elm make "src/Main.elm" --optimize --output="docs/elm.js"
